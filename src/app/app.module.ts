c74d487c94b17df9f1aa84ea23c05850f33e3148
Ann-Annice <annannice98@gmail.com>
2022-10-12 16:52:27 +0530

    shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
